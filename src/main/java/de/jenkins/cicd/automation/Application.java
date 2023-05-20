17ee117fbedf444ed149d3df7ceb6b519d21bf5f
sbhadani98 <shaileshkumarbhadani@gmail.com>
2023-05-20 11:56:33 +0200

    updated application.properties

--- src/main/java/de/jenkins/cicd/automation/Application.java
+++ src/main/java/de/jenkins/cicd/automation/Application.java
@@ -13,7 +13,7 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@GetMapping
+	@GetMapping("/")
 	public String greetings(){
 		return "Welcome to Jenkins CI/CD";
 	}
